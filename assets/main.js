400a6391fae086eee8c5bfe61944459a6b5b26ce
nico-personal <nico.cambelisa@gmail.com>
2020-06-04 23:23:44 +0800

    commit

--- assets/main.js
+++ assets/main.js
@@ -192,20 +192,20 @@
 			navigation: true,
 			responsive: {
 				0: {
-					items: 2,
+					items: 1,
 					nav: true,
 				},
 				600: {
-					items: 3,
+					items: 2,
 					nav: false,
 				},
 				1000: {
-					items: 4,
+					items: 3,
 					nav: true,
 					loop: false,
 				},
 				1400: {
-					items: 5,
+					items: 4,
 					nav: true,
 					loop: false,
 				},
